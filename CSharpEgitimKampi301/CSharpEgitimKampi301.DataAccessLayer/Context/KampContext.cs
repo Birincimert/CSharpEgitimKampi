e1ad4b9b5ec774dcf79595d56fb1b44998fbb596
Mert Yılmaz Birinci <mertbirinciofficial@gmail.com>
2024-12-28 13:11:04 +0300

    301

---
+++ CSharpEgitimKampi301/CSharpEgitimKampi301.DataAccessLayer/Context/KampContext.cs
@@ -0,0 +1,23 @@
+﻿using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Context
+{
+    public class KampContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; } //tırnak arasındaki ifade c#da kullanılacak sınıfın ismi, diğer Categories ise SQL'e yansıyacak tablo ismi
+                                                        //bunlar birbirine karışmasın diye yalın hali class, çoğul hali tablo olsun mantığı vardır.
+        public DbSet<Product> Products { get; set; }   //product'ı kullanmamızı sağlayan şey referans vermiş olmamız.
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Customer> Customers { get; set; } 
+
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
---
+++ CSharpEgitimKampi301/CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        //Table->Query ---------------->  Class->Table ve Property->Column
+
+        //Field-Variable-Property
+
+        //Class'ın içinde direkt olarak tanımlanırsa Field = public class A {  int x;  } yazmak gibi
+        //Class'ın içnide get ve setle beraber tanımlanırsa Property = public class A { public int y { get; set; } }
+        //Metot içinde tanımlanırsa da Variable olur. = void test () { int z; }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+
+
+    }
+}
